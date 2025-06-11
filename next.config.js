cc0dba158ed704d4acbb01d8b4d9c4bdcb607748
Ruhan Nandalal <ruhannandalal.in@gmail.com>
2025-06-11 21:12:16 +0530

    Fixed dependency errors

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
-module.exports = {
-  i18n: {
-    locales: ['en'],
-    defaultLocale: "en",
-  },
+const createNextIntlPlugin = require('next-intl/plugin');
+
+const withNextIntl = createNextIntlPlugin('./i18n.js');
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   output: 'standalone',
-}
+};
+
+module.exports = withNextIntl(nextConfig);
